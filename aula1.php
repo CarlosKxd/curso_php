809adf01e18d4fd03d0a6ef23e27d816f8f6cd01
CarlosKxd <carloseduardooliveirat16@gmail.com>
2024-06-18 21:57:38 -0300

    Primeiro commit no github

---
+++ aula1.php
@@ -0,0 +1,26 @@
+<?php
+
+//Este é um comentário.
+#Este também é um comentário.
+/* Este também é um comentário. */
+
+echo "Bueno<br>";
+$texto = "Pequeno texto que você está lendo sobre o livro do menino do maluquinho<br>";
+
+echo $texto;
+echo "Outro texto<br>";
+
+$outraVariavel = "Outro grande texto<br>"; #Aqui é um comentário.
+echo $outraVariavel;
+
+echo "Cálculos matemáticos<br>";
+echo 1+1;
+echo "<br>";
+echo 10-8;
+echo "<br>";
+echo 99*180;
+echo "<br>";
+echo 0/0;
+echo "<br>";
+echo 5+2;
+?>
