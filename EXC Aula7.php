b362e5817046bb90a6139713ed34bd14914c3e88
CarlosKxd <116656956+CarlosKxd@users.noreply.github.com>
2024-08-06 22:01:01 -0300

    Create EXC Aula7.php
    Identificar o tipo de triangulo conforme informado pelo usuário.
    Sendo três medidas: $lado1. $lado2. @lado3.
    De triangulo que é formado pelas medidas que ele passou.
    Triangulos a considerar: Equilatero, Isoceles ou escaleno.
    As medidas sou eu que irei informar e dependendo dos valores que eu informei, baseado nos conceitos matemáticos
    terei que dizer se tais medidas equivalem a Equilatero, Isoceles ou Escaleno.

---
+++ EXC Aula7.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Identificar o tipo de triangulo conforme informado pelo usuário.
+Sendo três medidas: $lado1. $lado2. @lado3.
+De triangulo que é formado pelas medidas que ele passou.
+Triangulos a considerar: Equilatero, Isoceles ou escaleno.
+As medidas sou eu que irei informar e dependendo dos valores que eu informei, baseado nos conceitos matemáticos
+terei que dizer se tais medidas equivalem a Equilatero, Isoceles ou Escaleno.
+*/
+
+$lado1=10;
+$lado2=15;
+$lado3=20;
+
+if $lado1
