22fafd8bfecb4aed495e2d19852f226f959be633
CarlosKxd <116656956+CarlosKxd@users.noreply.github.com>
2024-08-27 19:49:21 -0300

    Create teste.php

---
+++ teste.php
@@ -0,0 +1,41 @@
+<?php
+
+function validaCPF($cpf) {
+ 
+    // Extrai somente os números
+    $cpf = preg_replace( '/[^0-9]/is', '', $cpf );
+     
+    // Verifica se foi informado todos os digitos corretamente
+    if (strlen($cpf) != 11) {
+        return false;
+    }
+
+    // Verifica se foi informada uma sequência de digitos repetidos. Ex: 111.111.111-11
+    if (preg_match('/(\d)\1{10}/', $cpf)) {
+        return false;
+    }
+
+    // Faz o calculo para validar o CPF
+    for ($t = 9; $t < 11; $t++) {
+        for ($d = 0, $c = 0; $c < $t; $c++) {
+            $d += $cpf[$c] * (($t + 1) - $c);
+        }
+        $d = ((10 * $d) % 11) % 10;
+        if ($cpf[$c] != $d) {
+            return false;
+        }
+    }
+    return true;
+
+}
+
+$cpf = "491.582.630-98";
+
+
+$cpfValido = validaCPF($cpf);
+
+if ($cpfValido) {
+    echo "O CPF: $cpf é Valido.";
+} else {
+    echo "O CPF: $cpf é Invalido.";
+}
